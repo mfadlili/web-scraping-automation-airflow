a67168526ac9308f61b262fb7a15fc10acf9245a
John Doe <fadlil.ismail@gmail.com>
2022-10-26 23:57:49 +0700

    revision

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# web-scraping-automation-airflow
+# Web Scraping Automation using Airflow
+This repository is part of the following tutorial:
+https://medium.com/@fadlil.ismail/web-scraping-automation-using-apache-airflow-d8c02f3b1a12
+
+## Requirements
+To run this Airflow DAG, you need to install the following programs on Windows Subsystem for Linux (I used Ubuntu 22.04):
+1. python3
+2. python3-pip
+3. Postgresql
+
+After installing all the programs above, you need to install Apache Airflow on your WSL and activate it.
+You can follow this tutorial to install and setting up the Airflow : https://www.youtube.com/watch?v=Va_NMDoDqLQ&t=680s
+But don't forget to set AIRFLOW_HOME using this command :
+
+'export AIRFLOW_HOME=~/airflow' (quote signs not included)
+
+before you run 'airflow db init' (quote signs not included)
+
+You can find the airflow directory on this path :
+
+\\wsl$\Ubuntu-22.04\home\{your wsl username}\airflow
+
+or by running this command on your WSL : 'explorer.exe .' (quote signs not included)
+
+You can put the DAG file in the 'dags' folder, if the 'dags' folder doesn't exist you can create it first.
+
---
+++ second_dag.py
@@ -0,0 +1,99 @@
+import datetime as dt
+from datetime import timedelta
+import requests
+from bs4 import BeautifulSoup
+import psycopg2 as db
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.operators.email_operator import EmailOperator
+
+dict_month = {'Januari':1,
+              'Februari':2,
+              'Maret':3,
+              'April':4,
+              'Mei':5,
+              'Juni':6,
+              'Juli':7,
+              'Agustus':8,
+              'September':9,
+              'Oktober':10,
+              'November':11,
+              'Desember':12}
+            
+def scraping(ti): 
+    data = requests.get('https://www.bmkg.go.id/')
+    soup = BeautifulSoup(data.text, 'html.parser')
+    dict_result = {}
+    dict_result['Pukul'] = soup.find('span', {'class':'waktu'}).string.split(',')[1].split(' ')[1]
+    dict_result['Tanggal'] = soup.find('span', {'class':'waktu'}).string.split(',')[0]
+    dict_result['Tanggal'] = dict_result['Tanggal'].split(' ')[-1]+'-'+str(dict_month[dict_result['Tanggal'].split(' ')[-2]])+'-'+dict_result['Tanggal'].split(' ')[-3]
+    count = 0
+    for i in soup.find('ul', {'class':'list-unstyled'}).findChildren('li'):
+        if count==1:
+            dict_result["Magnitudo"] = float(i.text)
+        elif count==2:
+            dict_result["Kedalaman"] = int(i.text.split(" ")[0])
+        elif count==3:
+            dict_result["Koordinat"] = i.text
+        elif count==4:
+            dict_result["Lokasi"] = i.text
+        count += 1  
+
+    ti.xcom_push(key = 'time', value = dict_result['Tanggal']+' '+dict_result['Pukul'])
+    ti.xcom_push(key = 'magnitudo', value = dict_result["Magnitudo"])
+    ti.xcom_push(key = 'kedalaman', value = dict_result["Kedalaman"])
+    ti.xcom_push(key = 'koordinat', value = dict_result["Koordinat"])
+    ti.xcom_push(key = 'lokasi', value = dict_result["Lokasi"])
+
+def get_result(ti):
+    conn_string="dbname='postgres' host='localhost' user='postgres' password='postgres'"
+    conn=db.connect(conn_string)
+    cur=conn.cursor()
+    result_scraping = ti.xcom_pull(task_ids='scraping', key='time')
+    query2 = "select * from indonesian_earthquakes where datetime ="+ f"'{result_scraping}'"
+    cur.execute(query2)
+    if cur.fetchall():
+        return 'old_data'
+    else:
+        return 'new_data'
+
+
+default_args = {
+    'owner': 'Fadlil',
+    'start_date': dt.datetime(2022, 10, 26),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=5),
+}
+
+
+with DAG('ScrapingDAG',
+         default_args=default_args,
+         schedule_interval=timedelta(minutes=5),      # '0 * * * *',
+         ) as dag:
+
+    print_starting = BashOperator(task_id='starting',
+                               bash_command='echo "I am reading the CSV now....."')
+    
+    scraping_bmkg = PythonOperator(task_id='scraping',
+                                 python_callable=scraping)
+
+    get_result_bmkg = BranchPythonOperator(task_id='get_result',
+                                 python_callable=get_result)
+
+    bash2 = BashOperator(task_id='new_data',
+                               bash_command='echo "We get new data"')
+    
+    bash3 = BashOperator(task_id='old_data',
+                               bash_command='echo "there is old data"')
+    
+    send_email_to = EmailOperator(
+                                task_id='send_email_to_perkapgelex18',
+                                to='perkapgelex18@gmail.com',
+                                subject='New Earthquake in Indonesia',
+                                html_content='Attached is the latest sales JSON file as requested.')
+
+print_starting >> scraping_bmkg >> get_result_bmkg
+get_result_bmkg >> bash2 >> send_email_to
+get_result_bmkg >> bash3
